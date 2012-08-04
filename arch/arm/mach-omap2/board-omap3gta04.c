f272cf567d4bad609ddf2c8a467a637453533ef4
NeilBrown <neilb@suse.de>
2012-08-04 21:36:48 +1000

    BMP085: Clean up and enable use of interrupt for completion.
    - discard init_platform_hw and exit_platform_hw as they aren't
      used
    - pass GPIO or IRQ to driver and have it initialise the GPIO and
      the IRQ
    - finish waiting early if interrupt fires
    - clean up GPIO and IRQ on exit.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- arch/arm/mach-omap2/board-omap3gta04.c
+++ arch/arm/mach-omap2/board-omap3gta04.c
@@ -910,37 +910,8 @@ struct tsc2007_platform_data tsc2007_info = {
 
 #define BMP085_EOC_IRQ_GPIO		113	/* BMP085 end of conversion GPIO */
 
-static int __init bmp085_init(void)
-{
-	printk("bmp085_init()\n");
-	omap_mux_init_gpio(BMP085_EOC_IRQ_GPIO, OMAP_PIN_INPUT_PULLUP);
-	if (gpio_request(BMP085_EOC_IRQ_GPIO, "bmp085_eoc_irq")) {
-		printk(KERN_ERR "Failed to request GPIO %d for "
-			   "BMP085 EOC IRQ\n", BMP085_EOC_IRQ_GPIO);
-		return  -ENODEV;
-	}
-
-	if (gpio_direction_input(BMP085_EOC_IRQ_GPIO)) {
-		printk(KERN_WARNING "GPIO#%d cannot be configured as "
-			   "input\n", BMP085_EOC_IRQ_GPIO);
-		return -ENXIO;
-	}
-//	gpio_export(BMP085_EOC_IRQ_GPIO, 0);
-// 	omap_set_gpio_debounce(BMP085_EOC_IRQ_GPIO, 1);
-// 	omap_set_gpio_debounce_time(BMP085_EOC_IRQ_GPIO, 0xa);
-	gpio_set_debounce(BMP085_EOC_IRQ_GPIO, (0xa+1)*31);
-	irq_set_irq_type(OMAP_GPIO_IRQ(BMP085_EOC_IRQ_GPIO), IRQ_TYPE_EDGE_FALLING);
-	return 0;
-}
-
-static void bmp085_exit(void)
-{
-	gpio_free(BMP085_EOC_IRQ_GPIO);
-}
-
 struct bmp085_platform_data bmp085_info = {
-	.init_platform_hw	= bmp085_init,
-	.exit_platform_hw	= bmp085_exit,
+	.gpio = BMP085_EOC_IRQ_GPIO,
 };
 
 #endif
