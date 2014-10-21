99fbd4e948fd14c2eb432690e54991d8dabdfc27
NeilBrown <neilb@suse.de>
2014-10-21 15:55:11 +1100

    Input: gpio_keys: Make sure wake-up buttons work.
    If a key is to be used for wake-up, we must not disable
    the interrupt during suspend.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -526,6 +526,8 @@ static int gpio_keys_setup_key(struct platform_device *pdev,
 	 */
 	if (!button->can_disable)
 		irqflags |= IRQF_SHARED;
+	if (button->wakeup)
+		irqflags |= IRQF_NO_SUSPEND;
 
 	error = devm_request_any_context_irq(&pdev->dev, bdata->irq,
 					     isr, irqflags, desc, bdata);
