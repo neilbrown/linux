ede46ba37bf1a9f4e32bf89140cd21709271af3f
NeilBrown <neil@brown.name>
2018-05-07 17:20:48 +1000

    LEDE ramips patch 101-mt7621-timer.patch

--- arch/mips/ralink/timer-gic.c
+++ arch/mips/ralink/timer-gic.c
@@ -3,12 +3,15 @@
 #include <linux/of.h>
 #include <linux/clk-provider.h>
 #include <linux/clocksource.h>
+#include <asm/time.h>
 
 #include "common.h"
 
 void __init plat_time_init(void)
 {
 	ralink_of_remap();
+
+	mips_hpt_frequency = 880000000 / 2;
 
 	of_clk_init(NULL);
 	clocksource_probe();
