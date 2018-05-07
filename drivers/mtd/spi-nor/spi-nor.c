35385d500d45919cb611c1eb24a8f989a607af30
NeilBrown <neil@brown.name>
2018-05-07 17:20:07 +1000

    LEDE generic patch 479-enable_mtd_has_lock_for_f25l32pa.patch
    mtd: spi-nor: Add lock/unlock support for f25l32pa
    
    This chip has write protection enabled on power-up,
    so this flag is necessary to support write operations.
    
    Signed-off-by: Victor Shyba <victor1984@riseup.net>
    Acked-by: Marek Vasut <marek.vasut@gmail.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -689,7 +689,7 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "en25s64",	INFO(0x1c3817, 0, 64 * 1024,  128, SECT_4K) },
 
 	/* ESMT */
-	{ "f25l32pa", INFO(0x8c2016, 0, 64 * 1024, 64, SECT_4K) },
+	{ "f25l32pa", INFO(0x8c2016, 0, 64 * 1024, 64, SECT_4K | SPI_NOR_HAS_LOCK) },
 	{ "f25l32qa", INFO(0x8c4116, 0, 64 * 1024, 64, SECT_4K) },
 	{ "f25l64qa", INFO(0x8c4117, 0, 64 * 1024, 128, SECT_4K) },
 
