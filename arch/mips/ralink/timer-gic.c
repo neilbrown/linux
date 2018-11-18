ac116c7d9656cd7740e28e50d9e0aba581971cc3
Weijie Gao <hackpascal@gmail.com>
2018-11-19 01:14:53 +0800

    ramips: fix cpu clock of mt7621 and add dt clk devices
    For a long time the mt7621 uses a fixed cpu clock which causes a problem
    if the cpu frequency is not 880MHz.
    
    This patch fixes the cpu clock calculation and adds the cpu/bus clkdev
    which will be used in dts.
    
    Signed-off-by: Weijie Gao <hackpascal@gmail.com>

--- arch/mips/ralink/timer-gic.c
+++ arch/mips/ralink/timer-gic.c
@@ -2,7 +2,7 @@
 
 #include <linux/of.h>
 #include <linux/clk-provider.h>
-#include <linux/clocksource.h>
+#include <asm/time.h>
 #include <asm/time.h>
 
 #include "common.h"
@@ -10,8 +10,9 @@
 void __init plat_time_init(void)
 {
 	ralink_of_remap();
-
-	mips_hpt_frequency = 880000000 / 2;
+	ralink_clk_init();
+	if (!mips_hpt_frequency)
+		mips_hpt_frequency = 880000000 / 2;
 
 	of_clk_init(NULL);
 	clocksource_probe();
