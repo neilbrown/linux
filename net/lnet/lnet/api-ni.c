bd4b949fa447846675f1332f617cc4dd705ed580
NeilBrown <neilb@suse.com>
2019-09-25 16:23:40 +1000

    lnet: introduce wait_var_event_warning.
    LNet has a recurring pattern of waiting for some variable to
    reach a particular value, and generating a warning every second that
    it hasn't.  I many cases the warning has a higher priority if the
    wait has been for a power-of-2 seconds.
    
    This patch embodies that pattern in a new macro
      wait_var_event_warning()
    
    and uses wake_up_var() to cause the wait to complete as soon as
    possible.

--- net/lnet/lnet/api-ni.c
+++ net/lnet/lnet/api-ni.c
@@ -1333,10 +1333,9 @@ lnet_ping_md_unlink(struct lnet_ping_buffer *pbuf,
 	LNetInvalidateMDHandle(ping_mdh);
 
 	/* NB the MD could be busy; this just starts the unlink */
-	while (atomic_read(&pbuf->pb_refcnt) > 1) {
-		CDEBUG(D_NET, "Still waiting for ping data MD to unlink\n");
-		schedule_timeout_idle(HZ);
-	}
+	wait_var_event_warning(&pbuf->pb_refcnt,
+			       atomic_read(&pbuf->pb_refcnt) <= 1,
+			       "Still waiting for ping data MD to unlink\n");
 }
 
 static void
@@ -1564,10 +1563,9 @@ static void lnet_push_target_fini(void)
 	LNetInvalidateMDHandle(&the_lnet.ln_push_target_md);
 
 	/* Wait for the unlink to complete. */
-	while (atomic_read(&the_lnet.ln_push_target->pb_refcnt) > 1) {
-		CDEBUG(D_NET, "Still waiting for ping data MD to unlink\n");
-		schedule_timeout_uninterruptible(HZ);
-	}
+	wait_var_event_warning(&the_lnet.ln_push_target->pb_refcnt,
+			       atomic_read(&the_lnet.ln_push_target->pb_refcnt) <= 1,
+			       "Still waiting for ping data MD to unlink\n");
 
 	lnet_ping_buffer_decref(the_lnet.ln_push_target);
 	the_lnet.ln_push_target = NULL;
