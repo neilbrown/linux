0099dc554a4191a341a2710788d25168d536a3f4
NeilBrown <neilb@suse.com>
2019-09-25 16:23:41 +1000

    lnet: use kernel_accept() rather than lnet_sock_accept()
    The only advantage of lnet_sock_accept() over kernel_accept()
    appears to be that it can be interrupted by calling
      sk->sk_state_change()
    
    kernel_accept() can be interrupted by sending a signal
    to the process, and as it is only used from a dedicated process
    we can easily use a signal to interrupt.
    
    So discard lnet_sock_accept(), use kernel_accept(), and send
    a signal.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- net/lnet/lnet/acceptor.c
+++ net/lnet/lnet/acceptor.c
@@ -43,6 +43,7 @@ static int accept_timeout = 5;
 static struct {
 	int			pta_shutdown;
 	struct socket		*pta_sock;
+	struct task_struct	*pta_task;
 	struct completion	pta_signal;
 	struct net		*pta_ns;
 } lnet_acceptor_state = {
@@ -330,6 +331,7 @@ lnet_acceptor(void *arg)
 	int secure = (int)((long)arg);
 
 	LASSERT(!lnet_acceptor_state.pta_sock);
+	allow_signal(SIGKILL);
 
 	rc = lnet_sock_listen(&lnet_acceptor_state.pta_sock,
 			      0, accept_port, accept_backlog,
@@ -355,9 +357,9 @@ lnet_acceptor(void *arg)
 		return rc;
 
 	while (!lnet_acceptor_state.pta_shutdown) {
-		rc = lnet_sock_accept(&newsock, lnet_acceptor_state.pta_sock);
+		rc = kernel_accept(lnet_acceptor_state.pta_sock, &newsock, 0);
 		if (rc) {
-			if (rc != -EAGAIN) {
+			if (rc != -EINTR && rc != -ERESTARTSYS) {
 				CWARN("Accept error %d: pausing...\n", rc);
 				schedule_timeout_uninterruptible(HZ);
 			}
@@ -471,6 +473,7 @@ lnet_acceptor_start(void)
 	if (!lnet_acceptor_state.pta_shutdown) {
 		/* started OK */
 		LASSERT(lnet_acceptor_state.pta_sock);
+		lnet_acceptor_state.pta_task = task;
 		return 0;
 	}
 
@@ -482,17 +485,14 @@ lnet_acceptor_start(void)
 void
 lnet_acceptor_stop(void)
 {
-	struct sock *sk;
-
 	if (lnet_acceptor_state.pta_shutdown) /* not running */
 		return;
 
 	lnet_acceptor_state.pta_shutdown = 1;
 
-	sk = lnet_acceptor_state.pta_sock->sk;
-
-	/* awake any sleepers using safe method */
-	sk->sk_state_change(sk);
+	/* Cause kernel_accept to abort */
+	send_sig(SIGKILL, lnet_acceptor_state.pta_task, 1);
+	lnet_acceptor_state.pta_task = NULL;
 
 	/* block until acceptor signals exit */
 	wait_for_completion(&lnet_acceptor_state.pta_signal);
