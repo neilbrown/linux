2386fb10ada09e59c2fe9f4612b1dfccaf013a54
NeilBrown <neilb@suse.com>
2019-09-25 16:23:45 +1000

    lnet: remove LNetEQPoll
    Thee are no longer any used for LNetEQPoll, to remove it
    and any mention of it, together with mention of some
    other interfaces that have previously been removed.
    
    Also remove the ln_eq_waitq which is no longer used.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- net/lnet/lnet/api-ni.c
+++ net/lnet/lnet/api-ni.c
@@ -256,7 +256,6 @@ lnet_init_locks(void)
 {
 	spin_lock_init(&the_lnet.ln_eq_wait_lock);
 	spin_lock_init(&the_lnet.ln_msg_resend_lock);
-	init_waitqueue_head(&the_lnet.ln_eq_waitq);
 	init_waitqueue_head(&the_lnet.ln_rc_waitq);
 	mutex_init(&the_lnet.ln_lnd_mutex);
 }
