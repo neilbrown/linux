32a89ea176426439f25889ff53f030dd884331cc
NeilBrown <neilb@suse.com>
2019-09-25 16:23:42 +1000

    lnet: socklnd: simplify locking in ksocknal_reaper()
    It is easier to understand locking when the lock is taken when needed,
    rather than when it is dropped when not wanted.
    
    So don't take the lock outside the loop, only inside the loop
    where needed.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- net/lnet/klnds/socklnd/socklnd_cb.c
+++ net/lnet/klnds/socklnd/socklnd_cb.c
@@ -2408,9 +2408,9 @@ ksocknal_reaper(void *arg)
 	INIT_LIST_HEAD(&enomem_conns);
 	init_wait(&wait);
 
-	spin_lock_bh(&ksocknal_data.ksnd_reaper_lock);
-
 	while (!ksocknal_data.ksnd_shuttingdown) {
+		spin_lock_bh(&ksocknal_data.ksnd_reaper_lock);
+
 		conn = list_first_entry_or_null(&ksocknal_data.ksnd_deathrow_conns,
 						struct ksock_conn, ksnc_list);
 		if (conn) {
@@ -2421,7 +2421,6 @@ ksocknal_reaper(void *arg)
 			ksocknal_terminate_conn(conn);
 			ksocknal_conn_decref(conn);
 
-			spin_lock_bh(&ksocknal_data.ksnd_reaper_lock);
 			continue;
 		}
 
@@ -2434,7 +2433,6 @@ ksocknal_reaper(void *arg)
 
 			ksocknal_destroy_conn(conn);
 
-			spin_lock_bh(&ksocknal_data.ksnd_reaper_lock);
 			continue;
 		}
 
@@ -2517,11 +2515,7 @@ ksocknal_reaper(void *arg)
 
 		set_current_state(TASK_RUNNING);
 		remove_wait_queue(&ksocknal_data.ksnd_reaper_waitq, &wait);
-
-		spin_lock_bh(&ksocknal_data.ksnd_reaper_lock);
 	}
-
-	spin_unlock_bh(&ksocknal_data.ksnd_reaper_lock);
 
 	ksocknal_thread_fini();
 	return 0;
