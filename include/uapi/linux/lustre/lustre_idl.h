77f434141c78774a4a3a88074ae069c9ac0b8e21
Qian Yingjin <qian@ddn.com>
2019-09-25 16:29:21 +1000

    lustre: lsom: Add an OBD_CONNECT2_LSOM connect flag
    Add an OBD_CONNECT2_LSOM connect flag so that clients do not send
    MDS_ATTR_LSIZE and MDS_ATTR_LBLOCKS flags to the old servers that
    do not support them.
    
    Reviewed-on: https://review.whamcloud.com/34343
    Lustre-commit: fdd2c5d3a6e5 ("LU-12021 lsom: Add an OBD_CONNECT2_LSOM connect flag")
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-12021
    Lustre-commit: 03caa311ca92 ("LU-12021 lsom: Add an OBD_CONNECT2_LSOM connect flag")
    Signed-off-by: Qian Yingjin <qian@ddn.com>
    Signed-off-by: Minh Diep <mdiep@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/34663
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- include/uapi/linux/lustre/lustre_idl.h
+++ include/uapi/linux/lustre/lustre_idl.h
@@ -804,6 +804,7 @@ struct ptlrpc_body_v2 {
 #define OBD_CONNECT2_LOCK_CONVERT	0x80ULL /* IBITS lock convert support */
 #define OBD_CONNECT2_SELINUX_POLICY	0x400ULL /* has client SELinux policy */
 #define OBD_CONNECT2_SELINUX_POLICY	0x400ULL /* has client SELinux policy */
+#define OBD_CONNECT2_LSOM		0x800ULL /* LSOM support */
 
 /* XXX README XXX:
  * Please DO NOT add flag values here before first ensuring that this same
