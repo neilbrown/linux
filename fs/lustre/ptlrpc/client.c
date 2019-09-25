f69835d75caa2413c14516ed63e6e1c3f15d9e09
NeilBrown <neilb@suse.com>
2019-09-25 16:23:30 +1000

    lustre: ptlrpc: don't use list_for_each_entry_safe unnecessarily.
    list_for_each_entry_safe() is only needed if the body of the
    loop might change the list, or if it might drop a lock that would
    otherwise prevent the list from being changed.
    
    When the body does neither of these, list_for_each_entry() should be
    preferred as it makes the behaviour of the loop more clear to readers.
    
    In each of the cases changed there, the list cannot change while the
    loop proceeds.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- fs/lustre/ptlrpc/client.c
+++ fs/lustre/ptlrpc/client.c
@@ -2923,7 +2923,7 @@ int ptlrpc_replay_req(struct ptlrpc_request *req)
  */
 void ptlrpc_abort_inflight(struct obd_import *imp)
 {
-	struct ptlrpc_request *req, *n;
+	struct ptlrpc_request *req;
 
 	/*
 	 * Make sure that no new requests get processed for this import.
@@ -2937,7 +2937,7 @@ void ptlrpc_abort_inflight(struct obd_import *imp)
 	 * locked?  Also, how do we know if the requests on the list are
 	 * being freed at this time?
 	 */
-	list_for_each_entry_safe(req, n, &imp->imp_sending_list, rq_list) {
+	list_for_each_entry(req, &imp->imp_sending_list, rq_list) {
 		DEBUG_REQ(D_RPCTRACE, req, "inflight");
 
 		spin_lock(&req->rq_lock);
@@ -2949,7 +2949,7 @@ void ptlrpc_abort_inflight(struct obd_import *imp)
 		spin_unlock(&req->rq_lock);
 	}
 
-	list_for_each_entry_safe(req, n, &imp->imp_delayed_list, rq_list) {
+	list_for_each_entry(req, &imp->imp_delayed_list, rq_list) {
 		DEBUG_REQ(D_RPCTRACE, req, "aborting waiting req");
 
 		spin_lock(&req->rq_lock);
@@ -2976,9 +2976,9 @@ void ptlrpc_abort_inflight(struct obd_import *imp)
  */
 void ptlrpc_abort_set(struct ptlrpc_request_set *set)
 {
-	struct ptlrpc_request *req, *tmp;
+	struct ptlrpc_request *req;
 
-	list_for_each_entry_safe(req, tmp, &set->set_requests, rq_set_chain) {
+	list_for_each_entry(req, &set->set_requests, rq_set_chain) {
 		spin_lock(&req->rq_lock);
 		if (req->rq_phase != RQ_PHASE_RPC) {
 			spin_unlock(&req->rq_lock);
