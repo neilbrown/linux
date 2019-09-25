3a6cdb5a558ddfab35b70a5212e029f401150091
NeilBrown <neilb@suse.com>
2019-09-25 16:23:27 +1000

    lnet: discard kvec option from lnet_libmd.
    The 'struct kvec' field in 'struct lnet_libmd' is no
    longer set - the memory is always presented as pages.
    So remove that option and the union that contained
    iov and kiov.
    
    Rename md_iov.kiov to  md_kiov.
    Discard all code that made use of md_iov.iov.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- include/linux/lnet/lib-types.h
+++ include/linux/lnet/lib-types.h
@@ -219,10 +219,7 @@ struct lnet_libmd {
 	struct lnet_rsp_tracker	*md_rspt_ptr;
 	struct lnet_eq		*md_eq;
 	struct lnet_handle_md	 md_bulk_handle;
-	union {
-		struct kvec	iov[LNET_MAX_IOV];
-		struct bio_vec	kiov[LNET_MAX_IOV];
-	} md_iov;
+	struct bio_vec		 md_kiov[LNET_MAX_IOV];
 };
 
 #define LNET_MD_FLAG_ZOMBIE		BIT(0)
