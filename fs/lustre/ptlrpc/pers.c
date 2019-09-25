880b8a172eee666563160d8a021eb8546ef8245b
NeilBrown <neilb@suse.com>
2019-09-25 16:23:26 +1000

    lustre: lnet: discard LNET_MD_IOVEC
    This flag is never set, so discard it.
    lnet_md memory descriptors are either a single virtual
    address, or a list of bvec.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- fs/lustre/ptlrpc/pers.c
+++ fs/lustre/ptlrpc/pers.c
@@ -50,7 +50,7 @@ void ptlrpc_fill_bulk_md(struct lnet_md *md, struct ptlrpc_bulk_desc *desc,
 
 	LASSERT(mdidx < desc->bd_md_max_brw);
 	LASSERT(desc->bd_iov_count <= PTLRPC_MAX_BRW_PAGES);
-	LASSERT(!(md->options & (LNET_MD_IOVEC | LNET_MD_KIOV)));
+	LASSERT(!(md->options & LNET_MD_KIOV));
 
 	md->length = max(0, desc->bd_iov_count - mdidx * LNET_MAX_IOV);
 	md->length = min_t(unsigned int, LNET_MAX_IOV, md->length);
--- include/uapi/linux/lnet/lnet-types.h
+++ include/uapi/linux/lnet/lnet-types.h
@@ -416,11 +416,6 @@ struct lnet_md {
 	 * the number of entries in the array. The length can't be bigger
 	 * than LNET_MAX_IOV. The struct bio_vec is used to describe page-based
 	 * fragments that are not necessarily mapped in virtual memory.
-	 * - LNET_MD_IOVEC bit set: The start field points to the starting
-	 * address of an array of struct iovec and the length field specifies
-	 * the number of entries in the array. The length can't be bigger
-	 * than LNET_MAX_IOV. The struct iovec is used to describe fragments
-	 * that have virtual addresses.
 	 * - Otherwise: The memory region is contiguous. The start field
 	 * specifies the starting address for the memory region and the
 	 * length field specifies its length.
@@ -475,14 +470,12 @@ struct lnet_md {
 	 *   The data sent in the REPLY serves as an implicit acknowledgment.
 	 * - LNET_MD_KIOV: The start and length fields specify an array of
 	 *   struct bio_vec.
-	 * - LNET_MD_IOVEC: The start and length fields specify an array of
-	 *   struct iovec.
 	 * - LNET_MD_MAX_SIZE: The max_size field is valid.
 	 * - LNET_MD_BULK_HANDLE: The bulk_handle field is valid.
 	 *
 	 * Note:
-	 * - LNET_MD_KIOV or LNET_MD_IOVEC allows for a scatter/gather
-	 *   capability for memory descriptors. They can't be both set.
+	 * - LNET_MD_KIOV allows for a scatter/gather
+	 *   capability for memory descriptors.
 	 * - When LNET_MD_MAX_SIZE is set, the total length of the memory
 	 *   region (i.e. sum of all fragment lengths) must not be less than
 	 *   \a max_size.
@@ -534,8 +527,6 @@ struct lnet_md {
 #define LNET_MD_TRUNCATE	(1 << 4)
 /** See lnet_md::options. */
 #define LNET_MD_ACK_DISABLE	(1 << 5)
-/** See lnet_md::options. */
-#define LNET_MD_IOVEC		(1 << 6)
 /** See lnet_md::options. */
 #define LNET_MD_MAX_SIZE	(1 << 7)
 /** See lnet_md::options. */
