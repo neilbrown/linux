b2b7fb5b67d9112a2a137750febdb9963133dbbb
NeilBrown <neilb@suse.com>
2019-09-25 16:23:29 +1000

    lnet: discard lnet_kvaddr_to_page
    This function is no longer used, so discard it.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- include/linux/lnet/lib-lnet.h
+++ include/linux/lnet/lib-lnet.h
@@ -626,7 +626,6 @@ void lnet_me_unlink(struct lnet_me *me);
 
 void lnet_md_unlink(struct lnet_libmd *md);
 void lnet_md_deconstruct(struct lnet_libmd *lmd, struct lnet_event *ev);
-struct page *lnet_kvaddr_to_page(unsigned long vaddr);
 int lnet_cpt_of_md(struct lnet_libmd *md, unsigned int offset);
 
 void lnet_register_lnd(struct lnet_lnd *lnd);
