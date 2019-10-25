57cd99b07c4e072f1be139cbb18d36f04a24404e
Patrick Farrell <pfarrell@whamcloud.com>
2019-10-25 14:38:58 +1100

    lustre: vvp: dirty pages with pagevec
    When doing i/o from multiple writers to a single file, the
    per-file page cache lock (the mapping lock) becomes a
    bottleneck.
    
    Most current uses are single page at a time.  This converts
    one prominent use, marking page as dirty, to use a pagevec.
    
    When many threads are writing to one file, this improves
    write performance by around 25%.
    
    This requires implementing our own version of the
    set_page_dirty-->__set_page_dirty_nobuffers functions.
    
    This was modeled on upstream tip of tree:
    v5.2-rc4-224-ge01e060fe0 (7/13/2019)
    
    The relevant code is unchanged since Linux 4.17, and has
    changed only minimally since before Linux 2.6.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-9920
    Lustre-commit: a7299cb012f8 ("LU-9920 vvp: dirty pages with pagevec")
    Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/28711
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Li Dongyang <dongyangli@ddn.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/lustre/llite/llite_lib.c
+++ fs/lustre/llite/llite_lib.c
@@ -2131,6 +2131,7 @@ void ll_delete_inode(struct inode *inode)
 	struct ll_inode_info *lli = ll_i2info(inode);
 	struct address_space *mapping = &inode->i_data;
 	unsigned long nrpages;
+	unsigned long flags;
 
 	if (S_ISREG(inode->i_mode) && lli->lli_clob) {
 		/* It is last chance to write out dirty pages,
@@ -2154,9 +2155,9 @@ void ll_delete_inode(struct inode *inode)
 	 */
 	nrpages = mapping->nrpages;
 	if (nrpages) {
-		xa_lock_irq(&mapping->i_pages);
+		xa_lock_irqsave(&mapping->i_pages, flags);
 		nrpages = mapping->nrpages;
-		xa_unlock_irq(&mapping->i_pages);
+		xa_unlock_irqrestore(&mapping->i_pages, flags);
 	} /* Workaround end */
 
 	LASSERTF(nrpages == 0,
