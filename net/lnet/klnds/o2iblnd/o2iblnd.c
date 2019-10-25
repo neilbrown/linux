fb1387c7113a8e84a4fcc59d80184ba8096baa0a
Chris Horn <hornc@cray.com>
2019-10-25 14:38:58 +1100

    lustre: o2ib: Record rc in debug log on startup failure
    Since kiblnd_startup() return -ENETDOWN on failure, let's record the
    rc value for the failure case in the debug log.
    
    Cray-bug-id: LUS-7935
    WC-bug-id: https://jira.whamcloud.com/browse/LU-12824
    Lustre-commit: 99f85541a685 ("LU-12824 o2ib: Record rc in debug log on startup failure")
    Signed-off-by: Chris Horn <hornc@cray.com>
    Reviewed-on: https://review.whamcloud.com/36325
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- net/lnet/klnds/o2iblnd/o2iblnd.c
+++ net/lnet/klnds/o2iblnd/o2iblnd.c
@@ -2784,10 +2784,10 @@ static int kiblnd_dev_start_threads(struct kib_dev *dev, u32 *cpts, int ncpts)
 
 static int kiblnd_startup(struct lnet_ni *ni)
 {
-	char *ifname;
+	char *ifname = NULL;
 	struct lnet_inetdev *ifaces = NULL;
 	struct kib_dev *ibdev = NULL;
-	struct kib_net *net;
+	struct kib_net *net = NULL;
 	unsigned long flags;
 	int rc;
 	int i;
@@ -2802,8 +2802,10 @@ static int kiblnd_startup(struct lnet_ni *ni)
 
 	net = kzalloc(sizeof(*net), GFP_NOFS);
 	ni->ni_data = net;
-	if (!net)
+	if (!net) {
+		rc = -ENOMEM;
 		goto net_failed;
+	}
 
 	net->ibn_incarnation = ktime_get_real_ns() / NSEC_PER_USEC;
 
@@ -2820,6 +2822,7 @@ static int kiblnd_startup(struct lnet_ni *ni)
 
 		if (ni->ni_interfaces[1]) {
 			CERROR("ko2iblnd: Multiple interfaces not supported\n");
+			rc = -EINVAL;
 			goto failed;
 		}
 
@@ -2830,6 +2833,7 @@ static int kiblnd_startup(struct lnet_ni *ni)
 
 	if (strlen(ifname) >= sizeof(ibdev->ibd_ifname)) {
 		CERROR("IPoIB interface name too long: %s\n", ifname);
+		rc = -E2BIG;
 		goto failed;
 	}
 
@@ -2904,7 +2908,9 @@ failed:
 net_failed:
 	kiblnd_shutdown(ni);
 
-	CDEBUG(D_NET, "%s failed\n", __func__);
+	CDEBUG(D_NET, "Configuration of device %s failed: rc = %d\n",
+	       ifname ? ifname : "", rc);
+
 	return -ENETDOWN;
 }
 
