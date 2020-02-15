fe3f003ca0ffc2602c2a4e1605139b5e16d8f8ac
Lai Siyao <lai.siyao@whamcloud.com>
2020-02-15 10:48:59 +0800

    lustre: lmv: simplify name to stripe mapping
    Handle layout change internally when mapping name to stripe:
    * Move layout changing related code into lmv name to stripe mapping
      so callers doesn't need to take care of the internals.
    * lmv_name_to_stripe_index() maps name in new layout, and
      lmv_name_to_stripe_index_old() in old layout.
    * rename lmv_migrate_existence_check() to lmv_old_layout_lookup()
      to support directory restripe in the future.
    * support layout changing directory in LFSCK.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-11025
    Lustre-commit: 703afd15fc93 ("LU-11025 lmv: simplify name to stripe mapping")
    Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/37711
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- include/uapi/linux/lustre/lustre_user.h
+++ include/uapi/linux/lustre/lustre_user.h
@@ -708,6 +708,7 @@ static inline bool lmv_is_known_hash_type(__u32 type)
 
 #define LMV_HASH_FLAG_BAD_TYPE		0x20000000
 #define LMV_HASH_FLAG_MIGRATION		0x80000000
+#define LMV_HASH_FLAG_LAYOUT_CHANGE	LMV_HASH_FLAG_MIGRATION
 
 struct lustre_foreign_type {
 	uint32_t lft_type;
