8d6d4b46461fa0e6d05e3797c2c0f66147a95d1f
Gu Zheng <gzheng@ddn.com>
2020-02-27 16:15:21 -0500

    lustre: uapi: fix building fail against Power9 little endian
    We use "%ll[dux]" for __u64 variable as an input/output modifier,
    this may cause building error on some architectures which use "long"
    for 64-bit types, for example, Power9 little endian.
    Here add necessary typecasting (long long/unsigned long long) to
    make the build correct.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-12705
    Lustre-commit: 4eddf36ac360 ("LU-12705 build: fix building fail against Power9 little endian")
    Signed-off-by: Gu Zheng <gzheng@ddn.com>
    Reviewed-on: https://review.whamcloud.com/36007
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Li Xi <lixi@ddn.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- include/uapi/linux/lustre/lustre_user.h
+++ include/uapi/linux/lustre/lustre_user.h
@@ -521,7 +521,7 @@ struct lu_extent {
 } __packed;
 
 #define DEXT "[%#llx, %#llx)"
-#define PEXT(ext) (ext)->e_start, (ext)->e_end
+#define PEXT(ext) (unsigned long long)(ext)->e_start, (unsigned long long)(ext)->e_end
 
 static inline bool lu_extent_is_overlapped(struct lu_extent *e1,
 					    struct lu_extent *e2)
