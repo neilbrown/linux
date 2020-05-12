87a815a3778644e85e14ee1e7a1244f9516d0d02
Sebastien Buisson <sbuisson@ddn.com>
2020-05-13 00:58:15 +0900

    lustre: sec: better struct sepol_downcall_data
    struct sepol_downcall_data is badly formed for several reasons:
    - it uses a __kernel_time_t field, which can be variably sized,
      depending on the size of __kernel_long_t. Replace it with a
      fixed-size __s64 type;
    - it has __u32 sdd_magic that is immediately before a potentially
      64-bit field, whereas the 64-bit fields in a structure should
      always be naturally aligned on 64-bit boundaries to avoid potential
      incompatibility in the structure definition;
    - it has __u16 sdd_sepol_len which may be followed by padding.
    
    So create a better struct sepol_downcall_data, while maintaining
    compatibility with 2.12 by keeping a struct sepol_downcall_data_old.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-13525
    Lustre-commit: 82b8cb5528f4 ("LU-13525 sec: better struct sepol_downcall_data")
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Reviewed-on: https://review.whamcloud.com/38580
    Reviewed-by: Olaf Faaland-LLNL <faaland1@llnl.gov>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- include/uapi/linux/lustre/lustre_user.h
+++ include/uapi/linux/lustre/lustre_user.h
@@ -983,7 +983,6 @@ static inline const char *qtype_name(int qtype)
 }
 
 #define IDENTITY_DOWNCALL_MAGIC 0x6d6dd629
-#define SEPOL_DOWNCALL_MAGIC 0x8b8bb842
 
 /* permission */
 #define N_PERMS_MAX	64
@@ -1005,10 +1004,21 @@ struct identity_downcall_data {
 	__u32			    idd_groups[0];
 };
 
-struct sepol_downcall_data {
+/* old interface struct is deprecated in 2.14 */
+#define SEPOL_DOWNCALL_MAGIC_OLD 0x8b8bb842
+struct sepol_downcall_data_old {
 	__u32		sdd_magic;
 	__s64		sdd_sepol_mtime;
 	__u16		sdd_sepol_len;
+	char		sdd_sepol[0];
+};
+
+#define SEPOL_DOWNCALL_MAGIC 0x8b8bb843
+struct sepol_downcall_data {
+	__u32		sdd_magic;
+	__u16		sdd_sepol_len;
+	__u16		sdd_padding1;
+	__s64		sdd_sepol_mtime;
 	char		sdd_sepol[0];
 };
 
