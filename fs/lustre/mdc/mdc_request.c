0cb24152f875a0763c1d8fd2ae014f45954fc27d
Hongchao Zhang <hongchao@whamcloud.com>
2020-05-20 00:21:41 +0800

    lustre: mdc: chlg device could be used after free
    There are some issue of the usage of dynamic devices used by
    the changelog in MDC, which could cause the device to be used
    after it is freed.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-13508
    Lustre-commit: 1e992e94eaf8 ("LU-13508 mdc: chlg device could be used after free")
    Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/38658
    Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- fs/lustre/mdc/mdc_request.c
+++ fs/lustre/mdc/mdc_request.c
@@ -2958,10 +2958,12 @@ static int __init mdc_init(void)
 	rc = class_register_type(&mdc_obd_ops, &mdc_md_ops,
 				 LUSTRE_MDC_NAME, &mdc_device_type);
 	if (rc)
-		goto out_dev;
+		goto out_class;
 
 	return 0;
 
+out_class:
+	class_destroy(mdc_changelog_class);
 out_dev:
 	unregister_chrdev_region(mdc_changelog_dev, MDC_CHANGELOG_DEV_COUNT);
 	return rc;
@@ -2969,9 +2971,10 @@ out_dev:
 
 static void /*__exit*/ mdc_exit(void)
 {
+	class_unregister_type(LUSTRE_MDC_NAME);
 	class_destroy(mdc_changelog_class);
 	unregister_chrdev_region(mdc_changelog_dev, MDC_CHANGELOG_DEV_COUNT);
-	class_unregister_type(LUSTRE_MDC_NAME);
+	idr_destroy(&mdc_changelog_minor_idr);
 }
 
 MODULE_AUTHOR("OpenSFS, Inc. <http://www.lustre.org/>");
