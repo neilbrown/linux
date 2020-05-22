cff07e6a516820c07de76481307ed6ae39f23edd
Chris Horn <chris.horn@hpe.com>
2020-05-21 20:49:53 -0500

    lustre: lnet: Set remote NI status in lnet_notify
    The gnilnd receives node health information asynchronous from any tx
    failure, so aliveness of lpni as reported by lnet_is_peer_ni_alive()
    may not match what LND is telling us. Use existing reset flag to
    set cached NI status down so we can be sure that remote NIs are
    correctly set down.
    
    HPE-bug-id: LUS-8897
    WC-bug-id: https://jira.whamcloud.com/browse/LU-13648
    Lustre-commit: 8010dbb66076 ("LU-13648 lnet: Set remote NI status in lnet_notify")
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Reviewed-on: https://review.whamcloud.com/38862
    Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
    Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- net/lnet/lnet/router.c
+++ net/lnet/lnet/router.c
@@ -1676,8 +1676,7 @@ lnet_notify(struct lnet_ni *ni, lnet_nid_t nid, bool alive, bool reset,
 
 	CDEBUG(D_NET, "%s notifying %s: %s\n",
 	       !ni ? "userspace" : libcfs_nid2str(ni->ni_nid),
-	       libcfs_nid2str(nid),
-	       alive ? "up" : "down");
+	       libcfs_nid2str(nid), alive ? "up" : "down");
 
 	if (ni &&
 	    LNET_NIDNET(ni->ni_nid) != LNET_NIDNET(nid)) {
@@ -1719,6 +1718,7 @@ lnet_notify(struct lnet_ni *ni, lnet_nid_t nid, bool alive, bool reset,
 
 	if (alive) {
 		if (reset) {
+			lpni->lpni_ns_status = LNET_NI_STATUS_UP;
 			lnet_set_lpni_healthv_locked(lpni,
 						     LNET_MAX_HEALTH_VALUE);
 		} else {
@@ -1729,6 +1729,8 @@ lnet_notify(struct lnet_ni *ni, lnet_nid_t nid, bool alive, bool reset,
 				lpni,
 				sensitivity ?: lnet_health_sensitivity);
 		}
+	} else if (reset) {
+		lpni->lpni_ns_status = LNET_NI_STATUS_DOWN;
 	}
 
 	/* recalculate aliveness */
