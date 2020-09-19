b63b12f5bbd77025b0b06ba33255c18f70047620
Amir Shehata <ashehata@whamcloud.com>
2020-09-19 01:38:07 -0700

    lustre: o2iblnd: Don't retry indefinitely
    If peer is down don't retry indefinitely. Use the retry_count
    parameter to restrict the number of retries. After which the
    connection fails and error is propagated up.
    
    This prevents long timeouts when mounting a file system with
    nodes which might have their NIDs configured in the FS, but the
    nodes have been taken offline.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-13972
    Lustre-commit: 7c8ad11ef08f ("LU-13972 o2iblnd: Don't retry indefinitely")
    Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/39981
    Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
    Reviewed-by: Chris Horn <chris.horn@hpe.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- net/lnet/klnds/o2iblnd/o2iblnd_cb.c
+++ net/lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -2188,6 +2188,9 @@ kiblnd_connreq_done(struct kib_conn *conn, int status)
 	/* connection established */
 	write_lock_irqsave(&kiblnd_data.kib_global_lock, flags);
 
+	/* reset retry count */
+	peer_ni->ibp_retries = 0;
+
 	conn->ibc_last_send = ktime_get();
 	kiblnd_set_conn_state(conn, IBLND_CONN_ESTABLISHED);
 	kiblnd_peer_alive(peer_ni);
@@ -2638,6 +2641,11 @@ kiblnd_check_reconnect(struct kib_conn *conn, int version,
 		goto out;
 	}
 
+	if (peer_ni->ibp_retries > *kiblnd_tunables.kib_retry_count) {
+		reason = "retry count exceeded due to no listener";
+		goto out;
+	}
+
 	switch (why) {
 	default:
 		reason = "Unknown";
@@ -2734,6 +2742,7 @@ kiblnd_rejected(struct kib_conn *conn, int reason, void *priv, int priv_nob)
 		break;
 
 	case IB_CM_REJ_INVALID_SERVICE_ID:
+		peer_ni->ibp_retries++;
 		kiblnd_check_reconnect(conn, IBLND_MSG_VERSION, 0,
 				       IBLND_REJECT_INVALID_SRV_ID, NULL);
 		CNETERR("%s rejected: no listener at %d\n",
