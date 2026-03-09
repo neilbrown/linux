348051e084b99d8177efb132a4104e708c0df818
NeilBrown <neil@brown.name>
2026-03-09 11:44:47 +1100

    efivarfs: use d_alloc_name()
    efivarfs() is one of the few remaining users of d_alloc().
    Other similar filesystems use d_alloc_name() in the same circumstances.
    Now that d_alloc_name() supports ->d_hash (providing that it never
    fails), change efivarfs to use that.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- fs/efivarfs/super.c
+++ fs/efivarfs/super.c
@@ -207,23 +207,6 @@ static const struct dentry_operations efivarfs_d_ops = {
 	.d_hash = efivarfs_d_hash,
 };
 
-static struct dentry *efivarfs_alloc_dentry(struct dentry *parent, char *name)
-{
-	struct qstr q = QSTR(name);
-	struct dentry *d;
-	int err;
-
-	err = efivarfs_d_hash(parent, &q);
-	if (err)
-		return ERR_PTR(err);
-
-	d = d_alloc(parent, &q);
-	if (d)
-		return d;
-
-	return ERR_PTR(-ENOMEM);
-}
-
 bool efivarfs_variable_is_present(efi_char16_t *variable_name,
 				  efi_guid_t *vendor, void *data)
 {
@@ -278,9 +261,9 @@ static int efivarfs_create_dentry(struct super_block *sb, efi_char16_t *name16,
 	memcpy(entry->var.VariableName, name16, name_size);
 	memcpy(&(entry->var.VendorGuid), &vendor, sizeof(efi_guid_t));
 
-	dentry = efivarfs_alloc_dentry(root, name);
-	if (IS_ERR(dentry)) {
-		err = PTR_ERR(dentry);
+	dentry = d_alloc_name(root, name);
+	if (!dentry) {
+		err = -ENOMEM;
 		goto fail_inode;
 	}
 
