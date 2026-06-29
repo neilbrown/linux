556b7d3a9d8c231314435f8144651c81b1732748
Jori Koolstra <jkoolstra@xs4all.nl>
2026-06-29 17:36:24 +0200

    vfs: lookup_open(): use vfs_create_no_perm()
    We can replace the code in the no create_error/negative dentry found
    from lookup case in lookup_open() with the vfs_create_no_perm() helper.
    
    Signed-off-by: Jori Koolstra <jkoolstra@xs4all.nl>

--- fs/namei.c
+++ fs/namei.c
@@ -4568,15 +4568,10 @@ retry:
 		goto out_dput;
 	}
 
-	error = try_break_deleg(dir_inode, LEASE_BREAK_DIR_CREATE, &delegated_inode);
+	error = vfs_create_no_perm(idmap, dentry, mode, &delegated_inode);
 	if (error)
 		goto out_dput;
 
-	error = dir_inode->i_op->create(idmap, dir_inode, dentry, mode);
-	if (error)
-		goto out_dput;
-
-	fsnotify_create(dir_inode, dentry);
 	file->f_mode |= FMODE_CREATED;
 
 out:
