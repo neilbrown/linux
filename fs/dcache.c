1c4187e2ef11b33df2fe939b4a651a0dfdd32af7
NeilBrown <neil@brown.name>
2026-07-27 08:02:26 +1000

    VFS: unify the source code for 2 d_lookup functions.
    dcache.c has two lookup functions:
     __d_lookup_rcu_op_compare() when the filesystem provide a comparison op
     __d_lookup_rcu() when dentry_cmp() can be used.
    
    These have similar structure and comments warning that it is important
    to keep them consistent.
    
    We can make use of inline functions and compiler optimisations to use a
    single function with boolean flags which is inlined into each of the
    places with different flags, which get optimised for the different
    cases.
    
    This will make future changes simpler.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2361,44 +2361,104 @@ bool d_same_name(const struct dentry *dentry, const struct dentry *parent,
 }
 EXPORT_SYMBOL_GPL(d_same_name);
 
-/*
- * This is __d_lookup_rcu() when the parent dentry has
- * DCACHE_OP_COMPARE, which makes things much nastier.
- */
-static noinline struct dentry *__d_lookup_rcu_op_compare(
+#define D_LOOKUP_OP_COMPARE	BIT(0) /* Only relevant with RCU_WALK */
+
+static __always_inline struct dentry *__do_d_lookup(
 	const struct dentry *parent,
 	const struct qstr *name,
-	unsigned *seqp)
+	unsigned *seqp,
+	const int flags)
 {
 	u64 hashlen = name->hash_len;
 	struct hlist_bl_head *b = d_hash(hashlen);
 	struct hlist_bl_node *node;
 	struct dentry *dentry;
+	const unsigned char *str = name->name;
 
+	/*
+	 * The hash list is protected using RCU.
+	 *
+	 * Carefully use d_seq in RCU mode, or d_lock in refcount mode
+	 * when comparing a candidate dentry, to avoid races with
+	 * d_move().
+	 *
+	 * It is possible that concurrent renames can mess up our list
+	 * walk here and result in missing our dentry, resulting in the
+	 * false-negative result. d_lookup() protects against concurrent
+	 * renames using rename_lock seqlock.
+	 *
+	 * See Documentation/filesystems/path-lookup.txt for more details.
+	 */
 	hlist_bl_for_each_entry_rcu(dentry, node, b, d_hash) {
-		int tlen;
-		const char *tname;
-		unsigned seq;
+		unsigned int seq = 0;
 
+		if (dentry->d_name.hash_len != hashlen)
+			continue;
 seqretry:
+		/*
+		 * The dentry sequence count protects us from
+		 * concurrent renames, and thus protects parent
+		 * and name fields.
+		 *
+		 * The caller must perform a seqcount check in
+		 * order to do anything useful with the returned
+		 * dentry.
+		 *
+		 * NOTE!  We do a "raw" seqcount_begin here.
+		 * That means that we don't wait for the
+		 * sequence count to stabilize if it is in the
+		 * middle of a sequence change.  If we do the
+		 * slow dentry compare, we will do seqretries
+		 * until it is stable, and if we end up with a
+		 * successful lookup, we actually want to exit
+		 * RCU lookup anyway.
+		 *
+		 * Note that raw_seqcount_begin still *does*
+		 * smp_rmb(), so we are still guaranteed
+		 * NUL-termination of ->d_name.name.
+		 */
 		seq = raw_seqcount_begin(&dentry->d_seq);
+
 		if (dentry->d_parent != parent)
 			continue;
+		/*
+		 * Check for the dentry being unhashed.
+		 *
+		 * As tempting as it is, we *can't* skip it because of a
+		 * race window between us finding the dentry before it
+		 * gets unhashed and loading the sequence counter or
+		 * taking spin_lock after unhashing is finished.
+		 *
+		 * We can at least predict on it.
+		 */
 		if (d_unhashed(dentry))
 			continue;
-		if (dentry->d_name.hash != hashlen_hash(hashlen))
-			continue;
-		tlen = dentry->d_name.len;
-		tname = dentry->d_name.name;
-		/* we want a consistent (name,len) pair */
-		if (read_seqcount_retry(&dentry->d_seq, seq)) {
-			cpu_relax();
-			goto seqretry;
+
+		if (flags & D_LOOKUP_OP_COMPARE) {
+			int tlen;
+			const char *tname;
+
+			tlen = dentry->d_name.len;
+			tname = dentry->d_name.name;
+			/* we want a consistent (name,len) pair */
+			if (read_seqcount_retry(&dentry->d_seq, seq)) {
+				cpu_relax();
+				goto seqretry;
+			}
+			if (parent->d_op->d_compare(dentry, tlen, tname, name) == 0) {
+				*seqp = seq;
+				return dentry;
+			}
+		} else {
+			/*
+			 * dentry_cmp() doesn't depend on stability so
+			 * we don't need seqretry as we do with op_compare.
+			 */
+			if (unlikely(dentry_cmp(dentry, str, hashlen_len(hashlen)) == 0)) {
+				*seqp = seq;
+				return dentry;
+			}
 		}
-		if (parent->d_op->d_compare(dentry, tlen, tname, name) != 0)
-			continue;
-		*seqp = seq;
-		return dentry;
 	}
 	return NULL;
 }
@@ -2433,77 +2493,10 @@ struct dentry *__d_lookup_rcu(const struct dentry *parent,
 				const struct qstr *name,
 				unsigned *seqp)
 {
-	u64 hashlen = name->hash_len;
-	const unsigned char *str = name->name;
-	struct hlist_bl_head *b = d_hash(hashlen);
-	struct hlist_bl_node *node;
-	struct dentry *dentry;
-
-	/*
-	 * Note: There is significant duplication with __d_lookup_rcu which is
-	 * required to prevent single threaded performance regressions
-	 * especially on architectures where smp_rmb (in seqcounts) are costly.
-	 * Keep the two functions in sync.
-	 */
-
 	if (unlikely(parent->d_flags & DCACHE_OP_COMPARE))
-		return __d_lookup_rcu_op_compare(parent, name, seqp);
-
-	/*
-	 * The hash list is protected using RCU.
-	 *
-	 * Carefully use d_seq when comparing a candidate dentry, to avoid
-	 * races with d_move().
-	 *
-	 * It is possible that concurrent renames can mess up our list
-	 * walk here and result in missing our dentry, resulting in the
-	 * false-negative result. d_lookup() protects against concurrent
-	 * renames using rename_lock seqlock.
-	 *
-	 * See Documentation/filesystems/path-lookup.txt for more details.
-	 */
-	hlist_bl_for_each_entry_rcu(dentry, node, b, d_hash) {
-		unsigned seq;
-
-		/*
-		 * The dentry sequence count protects us from concurrent
-		 * renames, and thus protects parent and name fields.
-		 *
-		 * The caller must perform a seqcount check in order
-		 * to do anything useful with the returned dentry.
-		 *
-		 * NOTE! We do a "raw" seqcount_begin here. That means that
-		 * we don't wait for the sequence count to stabilize if it
-		 * is in the middle of a sequence change. If we do the slow
-		 * dentry compare, we will do seqretries until it is stable,
-		 * and if we end up with a successful lookup, we actually
-		 * want to exit RCU lookup anyway.
-		 *
-		 * Note that raw_seqcount_begin still *does* smp_rmb(), so
-		 * we are still guaranteed NUL-termination of ->d_name.name.
-		 */
-		seq = raw_seqcount_begin(&dentry->d_seq);
-		if (dentry->d_parent != parent)
-			continue;
-		if (dentry->d_name.hash_len != hashlen)
-			continue;
-		if (unlikely(dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0))
-			continue;
-		/*
-		 * Check for the dentry being unhashed.
-		 *
-		 * As tempting as it is, we *can't* skip it because of a race window
-		 * between us finding the dentry before it gets unhashed and loading
-		 * the sequence counter after unhashing is finished.
-		 *
-		 * We can at least predict on it.
-		 */
-		if (unlikely(d_unhashed(dentry)))
-			continue;
-		*seqp = seq;
-		return dentry;
-	}
-	return NULL;
+		return __do_d_lookup(parent, name, seqp, D_LOOKUP_OP_COMPARE);
+	else
+		return __do_d_lookup(parent, name, seqp, 0);
 }
 
 /**
