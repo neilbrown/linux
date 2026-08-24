baaf3ee3be607e69a9edbc4b34cd9a9348968365
NeilBrown <neil@brown.name>
2026-08-24 19:09:00 +1000

    VFS: use __d_lookup_rcu() to implement __d_lookup()
    Rather than having a separate lookup loop for __d_lookup(), reuse
    __d_lookup_rcu(), and repeat if the found dentry isn't sufficient
    stable.
    
    As well as sharing code, this effectively replaces taking a spinlock for
    each dentry in the chain with sampling d_seq for each dentry, which
    should be faster in the common case.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2548,58 +2548,20 @@ struct dentry *d_lookup(const struct dentry *parent, const struct qstr *name)
  */
 struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 {
-	unsigned int hash = name->hash;
-	struct hlist_bl_head *b = d_hash(hash);
-	struct hlist_bl_node *node;
-	struct dentry *found = NULL;
-	struct dentry *dentry;
+	guard(rcu)();
+	while (1) {
+		struct dentry *dentry;
+		unsigned int seq;
 
-	/*
-	 * Note: There is significant duplication with __d_lookup_rcu which is
-	 * required to prevent single threaded performance regressions
-	 * especially on architectures where smp_rmb (in seqcounts) are costly.
-	 * Keep the two functions in sync.
-	 */
-
-	/*
-	 * The hash list is protected using RCU.
-	 *
-	 * Take d_lock when comparing a candidate dentry, to avoid races
-	 * with d_move().
-	 *
-	 * It is possible that concurrent renames can mess up our list
-	 * walk here and result in missing our dentry, resulting in the
-	 * false-negative result. d_lookup() protects against concurrent
-	 * renames using rename_lock seqlock.
-	 *
-	 * See Documentation/filesystems/path-lookup.txt for more details.
-	 */
-	rcu_read_lock();
-	
-	hlist_bl_for_each_entry_rcu(dentry, node, b, d_hash) {
-
-		if (dentry->d_name.hash != hash)
+		dentry = __d_lookup_rcu(parent, name, &seq);
+		if (!dentry)
+			return dentry;
+		if (!lockref_get_not_dead(&dentry->d_lockref))
 			continue;
-
-		spin_lock(&dentry->d_lock);
-		if (dentry->d_parent != parent)
-			goto next;
-		if (d_unhashed(dentry))
-			goto next;
-
-		if (!d_same_name(dentry, parent, name))
-			goto next;
-
-		dentry->d_lockref.count++;
-		found = dentry;
-		spin_unlock(&dentry->d_lock);
-		break;
-next:
-		spin_unlock(&dentry->d_lock);
- 	}
- 	rcu_read_unlock();
-
- 	return found;
+		if (!read_seqcount_retry(&dentry->d_seq, seq))
+			return dentry;
+		dput(dentry);
+	}
 }
 
 /**
